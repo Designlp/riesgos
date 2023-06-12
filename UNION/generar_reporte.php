7903e3358abf1fccc81d5e2269686cd4e781c913
Design <dmercadoff@gmail.com>
2023-06-12 01:58:26 -0400

    pdf

--- UNION/generar_reporte.php
+++ UNION/generar_reporte.php
@@ -3,8 +3,9 @@ session_start();
 require 'db.php';
 require '../vendor/autoload.php';
 
+
+
 use Dompdf\Dompdf;
-use Dompdf\Options;
 
 if (!isset($_SESSION['user_id'])) {
   header('Location: login.php');
@@ -19,7 +20,31 @@ $stmt->execute([$user_id]);
 $riesgos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Genera el contenido del PDF en HTML
-$html = '<h1>Reporte de Riesgos</h1><table><thead><tr><th>Nombre</th><th>Descripción</th><th>Impacto</th><th>Frecuencia</th><th>Resultado</th></tr></thead><tbody>';
+$html = '
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>riesgos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
+    <link rel="stylesheet" href="estilo.css" />
+    <link rel="stylesheet" href="tabla.css" />
+    <link rel="stylesheet" href="popup.css" />
+</head>
+<body>
+<h1>Reporte de Riesgos</h1>
+<table class="table-fill">
+<thead>
+<tr>
+<th>Nombre</th>
+<th>Descripción</th>
+<th>Impacto</th>
+<th>Frecuencia</th>
+<th>Resultado Inherente</th>
+</tr>
+</thead><tbody>
+</body>
+';
 
 foreach ($riesgos as $riesgo) {
     $html .= "<tr>
@@ -33,23 +58,17 @@ foreach ($riesgos as $riesgo) {
 
 $html .= '</tbody></table>';
 
-$htmlContent = file_get_contents('reporte.html');
+// Inicializa DOMPDF y carga el contenido HTML
+$dompdf = new Dompdf();
+$dompdf->loadHtml($html);
 
-// Creamos una nueva instancia de Dompdf
-$options = new Options();
-$options->set('isRemoteEnabled', TRUE);
-$dompdf = new Dompdf($options);
-
-// Cargamos el contenido HTML
-$dompdf->loadHtml($htmlContent);
-
-// Renderizamos el documento
+// Renderiza el documento y genera el PDF
 $dompdf->render();
+$output = $dompdf->output();
 
-// Guardamos el PDF generado
-$pdfOutput = $dompdf->output();
-file_put_contents('reporte.pdf', $pdfOutput);
+// Guarda el PDF en un archivo
+file_put_contents('reporte.pdf', $output);
 
-// Enviamos una respuesta JSON con la ubicación del PDF generado
+// Envía una respuesta JSON con la ubicación del PDF
 echo json_encode(['ubicacion' => 'reporte.pdf']);
 ?>
