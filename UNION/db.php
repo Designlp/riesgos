f9de65ccdcb3da05692e9da53aaa620276643189
Design <dmercadoff@gmail.com>
2023-06-12 02:37:19 -0400

    pdf

--- UNION/db.php
+++ UNION/db.php
@@ -4,9 +4,15 @@ $db   = 'u583014169_riesgos';
 $user = 'u583014169_riesgos';
 $pass = 'infUMSA19921963';
 
-$dsn = "mysql:host=$host;dbname=$db";
-$pdo = new PDO($dsn, $user, $pass);
+
+// Creamos la conexión
+$conn = new mysqli($host, $db, $user, $pass);
+
+// Comprobamos la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+// $dsn = "mysql:host=$host;dbname=$db";
+// $pdo = new PDO($dsn, $user, $pass);
 ?>
-
-
 
