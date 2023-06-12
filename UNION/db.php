b0c30a9eacc79bad46b34fe9bc60ef3accb0d81c
Design <dmercadoff@gmail.com>
2023-06-12 02:43:56 -0400

    pdf

--- UNION/db.php
+++ UNION/db.php
@@ -6,5 +6,16 @@ $pass = 'infUMSA19921963';
 
 $dsn = "mysql:host=$host;dbname=$db";
 $pdo = new PDO($dsn, $user, $pass);
+
+
+// Crea la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+  die("Error de conexión: " . $conn->connect_error);
+}
+
 ?>
+
 
