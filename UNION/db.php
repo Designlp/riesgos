256bccf17f2bc501c901537e511c5d412ee39eeb
Design <dmercadoff@gmail.com>
2023-06-12 02:43:13 -0400

    pdf

--- UNION/db.php
+++ UNION/db.php
@@ -1,16 +1,10 @@
 <?php
+$host = 'localhost';
+$db   = 'u583014169_riesgos';
+$user = 'u583014169_riesgos';
+$pass = 'infUMSA19921963';
 
-$servername = "localhost";
-$username = "u583014169_riesgos";
-$password = "infUMSA19921963";
-$dbname = "u583014169_riesgos";
-
-// Crea la conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verifica la conexión
-if ($conn->connect_error) {
-  die("Error de conexión: " . $conn->connect_error);
-}
-
+$dsn = "mysql:host=$host;dbname=$db";
+$pdo = new PDO($dsn, $user, $pass);
 ?>
+
